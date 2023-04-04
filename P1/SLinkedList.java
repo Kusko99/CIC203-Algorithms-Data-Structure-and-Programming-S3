311a200f3583631733bc361980fb4a65d3a44497
Kusko99 <99877193+Kusko99@users.noreply.github.com>
2023-04-04 15:23:59 -0300

    Add files via upload

---
+++ P1/SLinkedList.java
@@ -0,0 +1,10 @@
+public class SLinkedList {
+    protected Node head;    //nodo cabeça da lista
+    protected long size;    //número de nodos da lista
+    /** Construtor default que cria a lista vazia */
+    public SLinkedList(){
+        head = null;
+        size = 0;
+    }
+    //...os métodos da pesquisa e atualização vão aqui...
+}
