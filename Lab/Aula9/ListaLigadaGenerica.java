037e786a009c395a17ca67782a7651598f0b6832
kusko99 <fernandaofo120@gmail.com>
2023-05-15 08:18:49 -0300

    Criando Lista Ligada Generica

---
+++ Lab/Aula9/ListaLigadaGenerica.java
@@ -0,0 +1,22 @@
+public class ListaLigadaGenerica <T>{
+    private class No <T>{
+        private T info;
+        private No<T> proximo;
+        public  No (T info){
+            this.info = info;
+            this.proximo = null;
+        }
+        public T getInfo(){
+            return info;
+        }
+        public void setInfo(T info){
+            this.info = info;
+        }
+        public No<T> getProximo(){
+            return proximo;
+        }
+        public void setProximo(No<T> proximo){
+            this.proximo = proximo;
+        }
+    }
+}
