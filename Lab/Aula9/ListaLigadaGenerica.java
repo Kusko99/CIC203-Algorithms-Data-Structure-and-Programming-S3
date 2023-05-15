5ea0b42392744e953cb068aee718b1c7d8c06745
kusko99 <fernandaofo120@gmail.com>
2023-05-15 09:01:34 -0300

    Adicionando metodos

--- Lab/Aula9/ListaLigadaGenerica.java
+++ Lab/Aula9/ListaLigadaGenerica.java
@@ -18,5 +18,52 @@ public class ListaLigadaGenerica <T>{
         public void setProximo(No<T> proximo){
             this.proximo = proximo;
         }
+        @Override
+        public String toString(){
+            return "[" + info + "]->";
+        }
+    }
+    private No<T> primeiro;
+    private No<T> ultimo;
+    private int tamanho;
+    //construtor padrão - "zera tudo"
+    public No<T> getPrimeiro(){
+        return primeiro;
+    }
+    public void setPrimeiro(No<T> primeiro){
+        this.primeiro = primeiro;
+    }
+    public No<T> getUltimo(){
+        return ultimo;
+    }
+    public void setUltimo(No<T> ultimo){
+        this.ultimo = ultimo;
+    }
+    public boolean estaVazia(){
+        // return primeiro==null;
+        // return ultimo==null;
+        return tamanho==0;
+    }
+    public void insereInicio (T info){
+        No<T> novo = new No<T>(info);
+        if (estaVazia()){
+            ultimo = novo;
+        }else{
+            ultimo.setProximo(novo);
+            novo.setProximo(primeiro);
+        }
+        primeiro = novo;
+        tamanho ++;
+    }
+    public T removeInicio(){
+        T info = primeiro.getInfo();
+        if (tamanho == 1){
+            ultimo = null;
+        }else{
+            ultimo.setProximo(primeiro);
+        }
+        tamanho--;
+        primeiro = primeiro.getProximo();
+        return info;
     }
 }
