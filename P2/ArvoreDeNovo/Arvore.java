986bb670d80e65d7265acc76740a79415d1d00b9
Kusko99 <fernandaofo120@gmail.com>
2023-06-03 18:27:49 -0300

    Adicionando o metodos de exibição

--- P2/ArvoreDeNovo/Arvore.java
+++ P2/ArvoreDeNovo/Arvore.java
@@ -3,6 +3,11 @@ public class Arvore<TIPO extends Comparable>{
     public Arvore(){
         this.raiz = null;
     }
+    
+    public Elemento<TIPO> getRaiz() {
+        return raiz;
+    }
+
     public void adicionar(TIPO valor){
         Elemento<TIPO> novElemento = new Elemento<TIPO>(valor);
         if(raiz == null){
@@ -26,6 +31,29 @@ public class Arvore<TIPO extends Comparable>{
                     }
                 }
             }
+        }
+    }
+    public void emOrdem(Elemento<TIPO> atual){
+        if(atual != null){
+            emOrdem(atual.getEsquerda());
+            System.out.println(atual.getValor());
+            emOrdem(atual.getDireita());
+        }
+    }
+
+    public void preOrdem(Elemento<TIPO> atual){
+        if(atual != null){
+            System.out.println(atual.getValor());
+            preOrdem(atual.getEsquerda());
+            preOrdem(atual.getDireita());
+        }
+    }
+
+    public void posOrdem(Elemento<TIPO> atual){
+        if(atual != null){
+            posOrdem(atual.getEsquerda());
+            posOrdem(atual.getDireita());
+            System.out.println(atual.getValor());
         }
     }
 }
